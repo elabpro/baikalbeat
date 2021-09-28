a0a61c156fb910a1f7f45c8ac0bd717485b895d0
Eugene Arbatsky <eugine@elab.pro>
2021-09-28 09:00:56 +0200

    +dependencies

--- deps/build.sh
+++ deps/build.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+#git clone https://github.com/seznam/elasticlient.git
+#cd elasticsearch
+#git submodule update --init --recursive
+#cd ..
+#git clone https://github.com/mfontanini/cppkafka
+#git clone https://github.com/Tencent/rapidjson
 cd /usr/src/app/elasticlient/build && cmake .. && make && make install
 cd /usr/src/app/cppkafka/build && cmake .. && make && make install
 cd /usr/src/app/rapidjson/build && cmake .. && make && make install
